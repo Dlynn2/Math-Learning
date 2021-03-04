dbd5fd0b7b777e2196f8431542b9c89ed715509f
Zachary Schallenberger <zapps98@gmail.com>
2021-03-03 19:53:04 -0700

    Update db.js

--- Ampd/database/db.js
+++ Ampd/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 const db = {}
-const sequelize = new Sequelize("test", "root", "newpassword", {
+const sequelize = new Sequelize("test", "root", "pass", {
     host : 'localhost',
     dialect: 'mysql',
     operatersAliases: false,
