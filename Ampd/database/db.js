b0549e2781558ed19d37c04addb95ee71f43f8a9
Zachary Schallenberger <zapps98@gmail.com>
2021-04-20 20:47:40 -0600

    Update db.js
    This file should be in the .gitignore and not be updated to what our local dbs are

--- Ampd/database/db.js
+++ Ampd/database/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize")
 const db = {}
-const sequelize = new Sequelize("test", "root", "pass", {
-    host : 'localhost',
+const sequelize = new Sequelize("mathLearningDB", "Dylan","mysql", {
+    host : '159.89.159.138',
+    port : '3306',
     dialect: 'mysql',
     operatersAliases: false,
 
@@ -10,6 +11,10 @@ const sequelize = new Sequelize("test", "root", "pass", {
         min:0,
         acquire:30000,
         idle:10000
+    },
+
+    dialectOptions:{
+	socketPath: "/var/run/mysqld/mysqld.sock"
     }
 })
 //creates new instance of Sequelize that connects to the mathLearningDB
